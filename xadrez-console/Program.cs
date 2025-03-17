7857694948aecc80cd0d1c2958f951e6b41165e8
Tiago Coelho <tiagoalexandrecoelho@hotmail.com>
2025-03-17 18:20:02 +0000

    Inicio implementação do Tabuleiro

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using tabuleiro;
-
-namespace xadrez_console
+﻿namespace xadrez_console
 {
     class Program
     {
