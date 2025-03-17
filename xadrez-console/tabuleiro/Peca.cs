6c8c80d18666b660490fbb6029f6715d233ab5be
Tiago Coelho <tiagoalexandrecoelho@hotmail.com>
2025-03-17 18:41:54 +0000

    Inicio colocar peças no tabuleiro

--- xadrez-console/tabuleiro/Peca.cs
+++ xadrez-console/tabuleiro/Peca.cs
@@ -7,9 +7,9 @@
         public int qteMovimentos { get; protected set; }
         public Tabuleiro Tab { get; set; }
 
-        public Peca(Posicao posicao, Cor cor, Tabuleiro tab)
+        public Peca(Cor cor, Tabuleiro tab)
         {
-            Posicao = posicao;
+            Posicao = null;
             Cor = cor;
             Tab = tab;
             this.qteMovimentos = 0;
