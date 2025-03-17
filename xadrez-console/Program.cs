405e3599b3c246de0b4bc6c428b842848ef4b354
Tiago Coelho <tiagoalexandrecoelho@hotmail.com>
2025-03-17 18:28:57 +0000

    Inicio Implementação método imprimir Tabuleiro

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace xadrez_console
+﻿using System;
+using System.Dynamic;
+using tabuleiro;
+
+namespace xadrez_console
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            Tabuleiro tab = new Tabuleiro(8, 8);
+
+            Tela.ImprimirTabuleiro(tab);
+
+            Console.ReadLine();
+
+
         }
     }
 }
